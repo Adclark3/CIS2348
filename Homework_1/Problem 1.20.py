66a3dd682e8ed81b7abe26c845eafa7b8aa8f152
Adclark3 <70180545+Adclark3@users.noreply.github.com>
2021-09-17 20:33:05 -0500

    Add files via upload

---
+++ Homework_1/Problem 1.20.py
@@ -0,0 +1,18 @@
+# Avery Clark
+# 1907691
+
+user_num1 = int(input('Enter integer:\n'))
+
+print('You entered:', user_num1)
+
+squared_num = user_num1 * user_num1
+print(user_num1, 'squared is', squared_num)
+
+cubed_num = user_num1 * user_num1 * user_num1
+print('And', user_num1, 'cubed is', cubed_num, '!!')
+
+user_num2 = int(input('Enter another integer:\n'))
+user_add = user_num1 + user_num2
+print(user_num1, '+', user_num2, 'is', user_add)
+user_multiple = user_num1 * user_num2
+print(user_num1, '*', user_num2, 'is', user_multiple)
